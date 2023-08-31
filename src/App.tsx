9780af97d16562c266a30a4525390f3580895c5d
Devaraj <devarajperayil327@gmail.com>
2023-08-31 11:24:43 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <main className="text-3xl font-bold underline">
+    hello world!
+    this is my first Tailwind project
+    <p className="text-blue-700">paragraph</p>
+    </main>
+    );
+}
+
+export default App;
